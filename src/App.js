2255d7a03d14aae66d2ef3793e2cafe33bbf1b92
omaranBazna <omran4d@gmail.com>
2022-07-16 11:25:52 -0400

    add the header component to the App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import {Container ,Navbar ,NavbarBrand} from 'reactstrap';
-import NucampLogo from './app/assests/img/logo.png';
 
-import CampsiteCard from './features/campsites/CampsiteCard.js';
+import CampsitesList from './features/campsites/CampsitesList';
 
 import { CAMPSITES } from './app/shared/CAMPSITES';
-
+import Header from '../src/components/Header'
 function App() {
   return (
     <div className="App">
-     <Navbar dark color="primary" sticky="top" expand="md">
-         <Container>
-          <NavbarBrand href="/">
-              <img src={NucampLogo} alt="nucamp logo" />
-          </NavbarBrand>
-         </Container>
-      </Navbar> 
-      <CampsiteCard campsite={CAMPSITES[0]} />
+   <Header />
+      <CampsitesList />
     </div>
   );
 }
