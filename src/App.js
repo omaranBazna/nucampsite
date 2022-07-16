4cb145640bdcff178e6c3d2c8c6316f2eafbf2b4
omaranBazna <omran4d@gmail.com>
2022-07-16 12:20:57 -0400

    finishing assesment for workshop1 react

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import './App.css';
 import CampsitesList from './features/campsites/CampsitesList';
 
 import { CAMPSITES } from './app/shared/CAMPSITES';
-import Header from '../src/components/Header'
+import Header from '../src/components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
    <Header />
       <CampsitesList />
+      <Footer />
     </div>
   );
 }
