3265158199ad702230b6c2b641b002f3fec4884b
omaran bazna <omran4d@gmail.com>
2022-07-22 11:38:32 -0400

    correct the describtion spelling

--- src/features/campsites/CampsiteDetails.js
+++ src/features/campsites/CampsiteDetails.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Col, Row, Card, CardImg, CardBody } from "reactstrap";
 
 const CampsiteDetails = (props) => {
-  const { name, image, descriptio } = props.selected;
+  const { name, image, description } = props.selected;
 
   return (
     <Card>
       <CardImg top src={image} />
 
-      <CardBody>{descriptio}</CardBody>
+      <CardBody>{description}</CardBody>
     </Card>
   );
 };
