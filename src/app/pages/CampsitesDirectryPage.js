0ccc56cd144f05ae3ae3adeb07218d15d3fcace9
omaran bazna <omran4d@gmail.com>
2022-07-22 11:26:54 -0400

    create campsite directory page

---
+++ src/app/pages/CampsitesDirectryPage.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col, Button } from "reactstrap";
+import CampsitesList from "../../features/campsites/CampsitesList";
+import CampsiteDetails from "../../features/campsites/CampsiteDetails";
+import { selectRandomCampsite } from "../../features/campsites/campsitesSlice";
+
+export default CampistesDirectryPage = () => {
+  const selectedCampsite = selectRandomCampsite();
+  return (
+    <Container>
+      <Row>
+        <Col sm="5" md="7">
+          <CampsitesList> </CampsitesList>
+        </Col>
+        <Col sm="7" md="5">
+          <CampsiteDetails select={selectedCampsite}></CampsiteDetails>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
