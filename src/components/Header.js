211747e6009bdae0c348803b539cd6ca18a0ba47
omaran bazna <omran4d@gmail.com>
2022-07-22 23:00:32 -0400

    collapse the navbar menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,28 +17,30 @@ const Header = () => {
         <img src={NucampLogo} className="float-start" alt="nucamp logo" />
         <h1 className="mt-1">Nucamp</h1>
       </NavbarBrand>
-      <Nav className="ms-auto" navbar>
-        <NavItem>
-          <NavLink className="nav-link" to="/">
-            <i className="fa fa-home fa-lg" /> Home
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to="/campsites">
-            <i className="fa fa-list fa-lg" /> Directory
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to="/about">
-            <i className="fa fa-info fa-lg" /> About
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to="/contact">
-            <i className="fa fa-address-card fa-lg" /> Contact
-          </NavLink>
-        </NavItem>
-      </Nav>
+      <Collapse isOpen={false}>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              <i className="fa fa-home fa-lg" /> Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/campsites">
+              <i className="fa fa-list fa-lg" /> Directory
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/about">
+              <i className="fa fa-info fa-lg" /> About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/contact">
+              <i className="fa fa-address-card fa-lg" /> Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 };
