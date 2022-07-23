17765544b16064a036288a20c0bd8d2e165a768c
omaran bazna <omran4d@gmail.com>
2022-07-22 22:58:21 -0400

    sstyling the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,32 +5,38 @@ import {
   NavbarToggler,
   Nav,
   NavItem,
-  NavLink,
 } from "reactstrap";
+import { NavLink } from "react-router-dom";
 import NucampLogo from "../app/assests/img/logo.png";
 import { useState } from "react";
 
 const Header = () => {
   return (
     <Navbar dark color="primary" sticky="top" expand="md">
-      <NavbarBrand href="/">
-        <img src={NucampLogo} alt="nucamp logo" />
+      <NavbarBrand className="ms-5" href="/">
+        <img src={NucampLogo} className="float-start" alt="nucamp logo" />
+        <h1 className="mt-1">Nucamp</h1>
       </NavbarBrand>
       <Nav className="ms-auto" navbar>
         <NavItem>
           <NavLink className="nav-link" to="/">
-            <i className="fa fa-home fa-lg" />
-            Home
+            <i className="fa fa-home fa-lg" /> Home
           </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink className="nav-link" to="/campsites"></NavLink>
+          <NavLink className="nav-link" to="/campsites">
+            <i className="fa fa-list fa-lg" /> Directory
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink className="nav-link" to="/about"></NavLink>
+          <NavLink className="nav-link" to="/about">
+            <i className="fa fa-info fa-lg" /> About
+          </NavLink>
         </NavItem>
         <NavItem>
-          <NavLink className="nav-link" to="/contact"></NavLink>
+          <NavLink className="nav-link" to="/contact">
+            <i className="fa fa-address-card fa-lg" /> Contact
+          </NavLink>
         </NavItem>
       </Nav>
     </Navbar>
