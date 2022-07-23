7bb2a580d315505d77d56b4bd4c41cb5561f0a09
omaran bazna <omran4d@gmail.com>
2022-07-22 22:06:59 -0400

    creating the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,36 @@
-import {Navbar,NavbarBrand } from 'reactstrap';
-import NucampLogo from '../app/assests/img/logo.png';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import NucampLogo from "../app/assests/img/logo.png";
+import { useState } from "react";
 
-const Header=()=>{
-    return(
-        <Navbar dark color="primary" sticky="top" expand="md">
-        
-         <NavbarBrand href="/">
-             <img src={NucampLogo} alt="nucamp logo" />
-         </NavbarBrand>
-       
-     </Navbar> 
-
-    );
-}
+const Header = () => {
+  return (
+    <Navbar dark color="primary" sticky="top" expand="md">
+      <NavbarBrand href="/">
+        <img src={NucampLogo} alt="nucamp logo" />
+      </NavbarBrand>
+      <Nav className="ms-auto" navbar>
+        <NavItem>
+          <NavLink className="nav-link" to="/"></NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/"></NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/"></NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="nav-link" to="/"></NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  );
+};
 export default Header;
