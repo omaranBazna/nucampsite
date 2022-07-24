f30705026d14eec7bf3051661f16c5ebf4545bc6
omaran bazna <omran4d@gmail.com>
2022-07-24 19:51:07 -0400

    select the campsite by id in the campsite detailed page

--- src/app/pages/CampsiteDetailedPage.js
+++ src/app/pages/CampsiteDetailedPage.js
@@ -2,3 +2,9 @@ import { Col, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
 import { selectCampsiteById } from "../../features/campsites/campsitesSlice";
 import CampsiteDetails from "../../features/campsites/CampsiteDetails";
+
+const CampsiteDetailedPage = () => {
+  const { campsiteid } = useParams();
+  const campsite = selectCampsiteById(campsiteid);
+};
+export default CampsiteDetailedPage;
