5cdb7df097d8d1f7f5c3086261cf1b2c150fc9e6
omaran bazna <omran4d@gmail.com>
2022-07-24 19:49:31 -0400

    import the details component to the details page

--- src/app/pages/CampsiteDetailedPage.js
+++ src/app/pages/CampsiteDetailedPage.js
@@ -1,2 +1,4 @@
 import { Col, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
+import { selectCampsiteById } from "../../features/campsites/campsitesSlice";
+import CampsiteDetails from "../../features/campsites/CampsiteDetails";
