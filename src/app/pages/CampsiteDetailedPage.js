f063a0e899e743f12e6e3f42a69ee81de017e4c3
omaran bazna <omran4d@gmail.com>
2022-07-24 19:52:27 -0400

    add the campsite detailed componet in the campsite detailed page

--- src/app/pages/CampsiteDetailedPage.js
+++ src/app/pages/CampsiteDetailedPage.js
@@ -1,4 +1,4 @@
-import { Col, Row } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
 import { selectCampsiteById } from "../../features/campsites/campsitesSlice";
 import CampsiteDetails from "../../features/campsites/CampsiteDetails";
@@ -6,5 +6,12 @@ import CampsiteDetails from "../../features/campsites/CampsiteDetails";
 const CampsiteDetailedPage = () => {
   const { campsiteid } = useParams();
   const campsite = selectCampsiteById(campsiteid);
+  return (
+    <Container>
+      <Row>
+        <CampsiteDetails campsite={campsite} />
+      </Row>
+    </Container>
+  );
 };
 export default CampsiteDetailedPage;
