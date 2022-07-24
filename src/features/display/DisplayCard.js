dc8bbfde56c774640168164ebc2018328a23dac6
omaran bazna <omran4d@gmail.com>
2022-07-24 19:33:49 -0400

    add card to the display card

--- src/features/display/DisplayCard.js
+++ src/features/display/DisplayCard.js
@@ -1,4 +1,12 @@
 import { Card, CardBody, CardTitle, CardImg } from "reactstrap";
 
-const DisplayCard = () => {};
+const DisplayCard = ({ item }) => {
+  const { description, name, image } = item;
+
+  return (
+    <Card>
+      <CardImg src={image} alt={name}></CardImg>
+    </Card>
+  );
+};
 export default DisplayCard;
