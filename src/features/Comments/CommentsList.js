7c44153e037e74ab02cd6d8487d58920a1b6eccc
omaran bazna <omran4d@gmail.com>
2022-07-24 23:18:44 -0400

    finishing campsite list component

---
+++ src/features/Comments/CommentsList.js
@@ -0,0 +1,24 @@
+import { Col } from "reactstrap";
+import Comment from "./Comment";
+import { selectCommentByCampsiteId } from "../campsites/campsitesSlice";
+
+const CommentsList = ({ campsiteid }) => {
+  const comments = selectCommentByCampsiteId(campsiteid);
+  if (campsiteid && campsiteid.length > 0) {
+    return (
+      <Col md="5" className="m-1">
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return <Comment key={comment.id} comment={comment} />;
+        })}
+      </Col>
+    );
+  }
+
+  return (
+    <Col md="5" className="m-1">
+      There is no comment for this campsite yet
+    </Col>
+  );
+};
+export default CommentsList;
