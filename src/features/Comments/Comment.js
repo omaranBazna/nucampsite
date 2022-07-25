5c86522eedee0b21e9008c4b5760de09cc6e19e0
omaran bazna <omran4d@gmail.com>
2022-07-24 23:06:46 -0400

    creating the comment component

---
+++ src/features/Comments/Comment.js
@@ -0,0 +1,13 @@
+import { formatDate } from "../../utilites/formatDate";
+
+const Comment = ({ comment }) => {
+  const { text: commentText, rating, author, date } = comment;
+  return (
+    <p>
+      {commentText}
+      <br />
+      {rating} /5 stars -- {author} ,{formatDate(date)}
+    </p>
+  );
+};
+export default Comment;
