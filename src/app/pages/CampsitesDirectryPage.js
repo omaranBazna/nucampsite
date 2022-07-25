18b9ff91733bd65dd3d7f60e1a14ebda1de3a78c
omaran bazna <omran4d@gmail.com>
2022-07-24 22:41:30 -0400

    delete unneccery files from campsite directory page

--- src/app/pages/CampsitesDirectryPage.js
+++ src/app/pages/CampsitesDirectryPage.js
@@ -1,25 +1,9 @@
-import { Container, Row, Col, Button } from "reactstrap";
+import { Container } from "reactstrap";
 import CampsitesList from "../../features/campsites/CampsitesList";
-import CampsiteDetails from "../../features/campsites/CampsiteDetails";
-///import { selectRandomCampsite } from "../../features/campsites/campsitesSlice";
-import { selectCampsiteById } from "../../features/campsites/campsitesSlice";
-import React from "react";
-import { useState } from "react";
-
 const CampsitesDirectryPage = () => {
-  const [campsiteid, setCampsiteId] = useState(0);
-  const selectedCampsite = selectCampsiteById(campsiteid);
-
   return (
     <Container>
-      <Row>
-        <Col sm="5" md="7">
-          <CampsitesList setCampsiteId={setCampsiteId}> </CampsitesList>
-        </Col>
-        <Col sm="7" md="5">
-          <CampsiteDetails campsite={selectedCampsite}></CampsiteDetails>
-        </Col>
-      </Row>
+      <CampsitesList> </CampsitesList>
     </Container>
   );
 };
