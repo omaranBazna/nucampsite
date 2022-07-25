3d12b50a43e97717b1489d7d8c6aa22da88fb437
omaran bazna <omran4d@gmail.com>
2022-07-24 23:56:41 -0400

    create the sub header component

---
+++ src/components/SubHeader.js
@@ -0,0 +1,11 @@
+import { Col, Ro, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const SubHeader = ({ current, detail }) => {
+  return (
+    <Row>
+      <Col></Col>
+    </Row>
+  );
+};
+export default SubHeader;
