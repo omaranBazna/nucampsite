ba6f3982635ad4a34eafa7418ee578b27d17d40a
omaran bazna <omran4d@gmail.com>
2022-07-24 22:46:17 -0400

    embend the CampsiteCard inside a link

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -3,13 +3,15 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 const CampsiteCard = ({ campsite }) => {
   const { id, image, name } = campsite;
   return (
-    <Card>
-      <CardImg width="100%" src={image} alt={name} onClick={() => {}} />
-      <CardImgOverlay>
-        {" "}
-        <CardTitle>{props.campsite.name}</CardTitle>
-      </CardImgOverlay>
-    </Card>
+    <Link to="/">
+      <Card>
+        <CardImg width="100%" src={image} alt={name} />
+        <CardImgOverlay>
+          {" "}
+          <CardTitle>{name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </Link>
   );
 };
 export default CampsiteCard;
