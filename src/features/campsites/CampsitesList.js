68e4e57e53bcc6a084547c2925180655694cfcd0
omaran bazna <omran4d@gmail.com>
2022-07-24 22:42:46 -0400

    update campsite list component

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,20 +2,13 @@ import CampsiteCard from "./CampsiteCard";
 import { Col, Row } from "reactstrap";
 import { Controller } from "react-spring";
 import { selectAllCampsites } from "./campsitesSlice";
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
   const campsites = selectAllCampsites();
   return (
     <Row className="ms-auto">
       {campsites.map((campsite) => {
         return (
-          <Col
-            onClick={() => {
-              setCampsiteId(campsite.id);
-            }}
-            md="5"
-            className="m-4"
-            key={campsite.id}
-          >
+          <Col md="5" className="m-4" key={campsite.id}>
             <CampsiteCard campsite={campsite} />
           </Col>
         );
