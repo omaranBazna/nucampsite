a8a24cd3c2060090c55f77a22048bcd45a0acca0
omaran bazna <omran4d@gmail.com>
2022-07-25 00:06:09 -0400

    add the subheader to the pages

--- src/app/pages/CampsiteDetailedPage.js
+++ src/app/pages/CampsiteDetailedPage.js
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import { selectCampsiteById } from "../../features/campsites/campsitesSlice";
 import CampsiteDetails from "../../features/campsites/CampsiteDetails";
 import CommentsList from "../../features/Comments/CommentsList";
+import SubHeader from "../../components/SubHeader";
 const CampsiteDetailedPage = () => {
   const { campsiteid } = useParams();
   const campsite = selectCampsiteById(campsiteid);
   return (
     <Container>
+      <SubHeader current={campiste.id} detail={true} />
       <Row>
         <CampsiteDetails campsite={campsite} />
         <CommentsList campsiteid={campsiteid} />
--- src/app/pages/CampsitesDirectryPage.js
+++ src/app/pages/CampsitesDirectryPage.js
@@ -1,8 +1,10 @@
 import { Container } from "reactstrap";
 import CampsitesList from "../../features/campsites/CampsitesList";
+import SubHeader from "../../components/SubHeader";
 const CampsitesDirectryPage = () => {
   return (
     <Container>
+      <SubHeader current={"Directory"}></SubHeader>
       <CampsitesList> </CampsitesList>
     </Container>
   );
