7ded004e109320c7abf29ea87f616f68896abb51
omaran bazna <omran4d@gmail.com>
2022-07-28 02:46:39 -0400

    add the rest of the contact info

--- src/app/pages/ContactPage.js
+++ src/app/pages/ContactPage.js
@@ -16,6 +16,19 @@ const ContactPage = () => {
             U.S.A.
           </address>
         </Col>
+        <Col>
+          <a role="button" className="btn btn-link" href="tel:+12065551234">
+            <i className="fa fa-phone" /> 1-206-555-1234
+          </a>
+          <br />
+          <a
+            role="button"
+            className="btn btn-link"
+            href="mailto:fakeemail@fakeemail.co"
+          >
+            <i className="fa fa-envelope-o" /> campsites@nucamp.co
+          </a>
+        </Col>
       </Row>
       <Row className="row-content"></Row>
     </Container>
