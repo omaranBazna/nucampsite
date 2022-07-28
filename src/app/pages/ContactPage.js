62add8c98351e46d7aa5b8cd580d137cbfa87534
omaran bazna <omran4d@gmail.com>
2022-07-28 02:42:31 -0400

    return container in the contact page

--- src/app/pages/ContactPage.js
+++ src/app/pages/ContactPage.js
@@ -2,7 +2,7 @@ import { Row, Col, Container } from "reactstrap";
 import SubHeader from "../../components/SubHeader";
 
 const ContactPage = () => {
-  return <div>Contact Page</div>;
+  return <Container></Container>;
 };
 
 export default ContactPage;
