0c56687faf61050f78c5c72f9cc120d69a41b52c
omaran bazna <omran4d@gmail.com>
2022-07-28 23:36:06 -0400

    add the animation modules ,add the animation side effect

---
+++ src/features/display/AnimatedDisplayCard.js
@@ -0,0 +1,26 @@
+import { Card, CardBody, CardTitle, CardImg, CardText } from "reactstrap";
+import { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
+import { frameLoop } from "@react-spring/shared";
+const AnimatedDisplayCard = ({ item }) => {
+  const { description, name, image } = item;
+  const [toggle, setToggle] = useState(false);
+  const animatedstyle = useSpring({
+    opacity: toggle ? 1 : 0,
+    transform: toggle ? "scale(1,1)" : "scale(1,0)",
+    config: { duration: 500 },
+  });
+  useEffect(() => {
+    setToggle(true);
+  }, []);
+  return (
+    <Card>
+      <CardImg src={image} alt={name}></CardImg>
+      <CardBody>
+        <CardTitle>{name}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+    </Card>
+  );
+};
+export default AnimatedDisplayCard;
