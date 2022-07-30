4a7085b08359e3bc13abbef382d992ac7de453f4
omaran bazna <omran4d@gmail.com>
2022-07-30 10:48:13 -0400

    add comment form modal component

---
+++ src/components/CommentForm.js
@@ -0,0 +1,6 @@
+import { vButton, Modal, ModalHeader, ModalBody } from "reactstrap";
+import { useState } from "react";
+const CommentForm = () => {
+  return <></>;
+};
+export default CommentForm;
