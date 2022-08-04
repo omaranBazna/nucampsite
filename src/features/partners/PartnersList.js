bae861c8a12dd0d8987d8c31597d974f6e03308f
omaran bazna <omran4d@gmail.com>
2022-08-03 22:04:05 -0400

    invoke the useSelectro hook

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -3,7 +3,7 @@ import Partner from "./Partner";
 import { selectAllPartners } from "./partnersSlice";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 const PartnersList = () => {
-  const partners = selectAllPartners();
+  const partners = useSelector(selectAllPartners);
   return (
     <Col className="mt-4">
       {partners.map((partner) => {
