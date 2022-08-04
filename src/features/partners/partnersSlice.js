2df58895508dacba76a9f07721c644f8c940fad2
omaran bazna <omran4d@gmail.com>
2022-08-03 22:00:51 -0400

    add the initial state to the partner slice

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -1,5 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
 import { PARTNERS } from "../../app/shared/PARTNERS";
+const initialState = {
+  partnersArray: PARTNERS,
+};
+
 export const selectAllPartners = () => {
   return PARTNERS;
 };
