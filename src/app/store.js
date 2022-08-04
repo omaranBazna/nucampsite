3126e39d6e9b306122ad1596c880a3df8fd8ef37
omaran bazna <omran4d@gmail.com>
2022-08-03 22:16:57 -0400

    import comments reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { campsitesReducer } from "../features/campsites/campsitesSlice";
 import { partnersReducer } from "../features/partners/partnersSlice";
+import { commentsReducer } from "../features/Comments/commentsSlice";
 export const store = configureStore({
   reducer: {
     campsite: campsitesReducer,
