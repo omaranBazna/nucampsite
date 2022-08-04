9034ac6433646a2c10282b6e746c78cf24e977d5
omaran bazna <omran4d@gmail.com>
2022-08-03 22:11:12 -0400

    create teh promotion slice

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -5,6 +5,11 @@ import { PROMOTIONS } from "../../app/shared/PROMOTIONS";
 const initialState = {
   promotionsArray: PROMOTIONS,
 };
+
+const promotionsSlice = createSlice({
+  name: "promotions",
+  initialState,
+});
 export const selectFeuturedPromotion = () => {
   return PROMOTIONS.find((pro) => pro.featured);
 };
