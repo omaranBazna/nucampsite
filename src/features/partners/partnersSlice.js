4e31a7dc80403597005e828494baabede2f89ed7
omaran bazna <omran4d@gmail.com>
2022-08-03 22:03:04 -0400

    update the partnersSlice functions

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -4,15 +4,15 @@ const initialState = {
   partnersArray: PARTNERS,
 };
 const partnersSlice = createSlice({
-  name: "partner",
+  name: "partners",
   initialState,
 });
 export const partnersReducer = partnersSlice.reducer;
 
-export const selectAllPartners = () => {
-  return PARTNERS;
+export const selectAllPartners = (state) => {
+  return state.partners.partnersArray;
 };
 
-export const selectFeaturedPartner = () => {
-  return PARTNERS.find((partner) => partner.featured);
+export const selectFeaturedPartner = (state) => {
+  return state.partners.partnersArray.find((partner) => partner.featured);
 };
