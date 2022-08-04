0445310fae4a0476e7e96d9d21afada15d85fcfd
omaran bazna <omran4d@gmail.com>
2022-08-03 22:10:35 -0400

    initialize the initila state

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -2,6 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 import { PROMOTIONS } from "../../app/shared/PROMOTIONS";
 
+const initialState = {
+  promotionsArray: PROMOTIONS,
+};
 export const selectFeuturedPromotion = () => {
   return PROMOTIONS.find((pro) => pro.featured);
 };
