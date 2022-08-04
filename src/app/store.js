8948e6b286b7aa8cf4e332681badc03e44b68d58
omaran bazna <omran4d@gmail.com>
2022-08-03 21:17:04 -0400

    add the campsite reducer to the config of the sotre

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { campsitesReducer } from "../features/campsites/campsitesSlice";
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    campsite: campsitesReducer,
   },
 });
