498446535455325b65cccd740f42b844f5602991
omaran bazna <omran4d@gmail.com>
2022-08-03 21:51:07 -0400

    define the initial array of the comment slice

--- src/features/Comments/commentsSlice.js
+++ src/features/Comments/commentsSlice.js
@@ -2,6 +2,10 @@ import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
 import { COMMENTS } from "../../app/shared/COMMENTS";
 
 import { isCompositeComponent } from "react-dom/test-utils";
+
+const initialState = {
+  commentsArray: COMMENTS,
+};
 export const selectCommentByCampsiteId = (campsiteid) => {
   return COMMENTS.filter((comment) => {
     return comment.campsiteId == campsiteid;
