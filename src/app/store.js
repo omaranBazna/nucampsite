1cf0f988d2bd72f413c8e8339e29ae29b89efc8d
omaran bazna <omran4d@gmail.com>
2022-08-03 22:17:32 -0400

    import promotions reducers to the store

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { campsitesReducer } from "../features/campsites/campsitesSlice";
 import { partnersReducer } from "../features/partners/partnersSlice";
 import { commentsReducer } from "../features/Comments/commentsSlice";
+import { promotionsReducer } from "../features/promotions/promotionsSlice";
 export const store = configureStore({
   reducer: {
     campsite: campsitesReducer,
