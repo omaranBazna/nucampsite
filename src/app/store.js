832d1430ef02557e05589949bef649d7d803a832
omaran bazna <omran4d@gmail.com>
2022-08-03 22:16:23 -0400

    import the partner reedcucers to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { campsitesReducer } from "../features/campsites/campsitesSlice";
+import { partnersReducer } from "../features/partners/partnersSlice";
 export const store = configureStore({
   reducer: {
     campsite: campsitesReducer,
