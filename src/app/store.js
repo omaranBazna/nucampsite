b2e49f57c78d2ac88f8b394838d9903edcf78b64
omaran bazna <omran4d@gmail.com>
2022-08-05 00:26:52 -0400

    import logger

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { campsitesReducer } from "../features/campsites/campsitesSlice";
 import { partnersReducer } from "../features/partners/partnersSlice";
 import { commentsReducer } from "../features/Comments/commentsSlice";
 import { promotionsReducer } from "../features/promotions/promotionsSlice";
+import logger from "redux-logger";
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
