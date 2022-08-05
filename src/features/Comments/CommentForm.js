2ad4ba43ba7bd8064265b4fc759fa4adcb05d2a8
omaran bazna <omran4d@gmail.com>
2022-08-04 21:57:49 -0400

    define the dispatch function

--- src/features/Comments/CommentForm.js
+++ src/features/Comments/CommentForm.js
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { addComment } from "./commentsSlice";
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const dispatch=useDispatch();
   const handleSubmit = (values) => {
     const comment = {
       campsiteId: parseInt(campsiteId),
@@ -16,6 +17,7 @@ const CommentForm = ({ campsiteId }) => {
       date:new Date(Date.now()).toISOString();
     };
     console.log(comment);
+
   };
   return (
     <>
