8aa4974d017e6687f2d8ad06c3f03bdb86964286
omaran bazna <omran4d@gmail.com>
2022-08-04 21:56:31 -0400

    import addComment action to the comment form

--- src/features/Comments/CommentForm.js
+++ src/features/Comments/CommentForm.js
@@ -4,6 +4,7 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import { FormGroup, Label } from "reactstrap";
 import { validateCommentForm } from "../../utils/validateCommentForm";
 import { useDispatch } from "react-redux";
+import { addComment } from "./commentsSlice";
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const handleSubmit = (values) => {
