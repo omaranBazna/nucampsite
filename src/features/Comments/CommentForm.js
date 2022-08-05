33c4bfa02a65947ff2687f0d8dc9a27363124d61
omaran bazna <omran4d@gmail.com>
2022-08-04 21:58:28 -0400

    dispacth the add comment action

--- src/features/Comments/CommentForm.js
+++ src/features/Comments/CommentForm.js
@@ -17,7 +17,7 @@ const CommentForm = ({ campsiteId }) => {
       date:new Date(Date.now()).toISOString();
     };
     console.log(comment);
-
+    dispatch(addComment(comment))
   };
   return (
     <>
