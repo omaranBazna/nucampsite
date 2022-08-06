1b001b238a264e144903bb9dba88a453c26e9cf8
omaran bazna <omran4d@gmail.com>
2022-08-06 11:13:58 -0400

    add the span to the log in modal

--- src/features/user/UserLoginForm.js
+++ src/features/user/UserLoginForm.js
@@ -1,17 +1,53 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setCurrentUser, selectCurrentUser } from './userSlice';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentUser, selectCurrentUser } from "./userSlice";
 import {
-    Modal,
-    ModalHeader,
-    ModalBody,
-    FormGroup,
-    Label,
-    Button
-} from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
-import defaultAvatar from '../../app/assets/img/unicorn.png';
-const UserLoginForm=()=>{
-    return()
-}
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Label,
+  Button,
+} from "reactstrap";
+import { Formik, Field, Form } from "formik";
+import defaultAvatar from "../../app/assets/img/unicorn.png";
+const UserLoginForm = () => {
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+  const handleLogin = (values) => {
+    const currentUser = {
+      id: Date.now(),
+      avatar: defaultAvatar,
+      username: values.username,
+      password: values.password,
+    };
+    dispatch(setCurrentUser(currentUser));
+    setLoginModalOpen(false);
+  };
+  return (
+    <>
+      <span className="navbar-text ml-auto">
+        {currentUser ? (
+          <div style={{ width: "4rem", height: "4rem" }}>
+            <img
+              src={currentUser.avatar}
+              alt={"user"}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+        ) : (
+          <Button
+            outline
+            onClick={() => setLoginModalOpen(true)}
+            style={{ color: "white", border: "1px solid white" }}
+          >
+            <i className="fa fa-sign-in fa-lg" /> Login
+          </Button>
+        )}
+      </span>
+      <Modal></Modal>
+    </>
+  );
+};
 export default UserLoginForm;
