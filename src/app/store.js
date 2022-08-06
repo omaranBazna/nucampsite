da5aeac9c4d65ff92f021caf366e9b038b2bedc4
omaran bazna <omran4d@gmail.com>
2022-08-06 11:04:30 -0400

    import the user reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { campsitesReducer } from "../features/campsites/campsitesSlice";
 import { partnersReducer } from "../features/partners/partnersSlice";
 import { commentsReducer } from "../features/Comments/commentsSlice";
 import { promotionsReducer } from "../features/promotions/promotionsSlice";
+import { userReducer } from "../features/user/userSlice";
 import logger from "redux-logger";
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     partners: partnersReducer,
     promotions: promotionsReducer,
     comments: commentsReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
 });
