855a1ca548e6e6701e387ae41dad7076b03b1176
omaran bazna <omran4d@gmail.com>
2022-08-06 11:46:31 -0400

    finish task 3

--- src/utils/validateUserLoginForm.js
+++ src/utils/validateUserLoginForm.js
@@ -6,13 +6,13 @@ export const validateUserLoginForm = (values) => {
   } else if (values.username.length < 6) {
     errors.username = "username should be at least six chars";
   } else if (values.username.length > 15) {
-    errors.username = "username should be at least fifteen chars";
+    errors.username = "username should be at most fifteen chars";
   }
 
   if (!values.password) {
     errors.password = "Required";
-  } else if (values.username.length < 8) {
-    errors.password = "username should be at least eight chars";
+  } else if (values.password.length < 8) {
+    errors.password = "password should be at least eight chars";
   }
 
   return errors;
