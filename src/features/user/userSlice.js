5415d26c4d49dbd19fa9ea0cc576811ec7f51072
omaran bazna <omran4d@gmail.com>
2022-08-06 10:58:08 -0400

    create the user slice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -3,3 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   currentUser: null,
 };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
