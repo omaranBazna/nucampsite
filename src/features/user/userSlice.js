5b4569baca6bf18e08f4749b8e6d1605f58b3d5a
omaran bazna <omran4d@gmail.com>
2022-08-06 11:03:24 -0400

    export select current user

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -13,3 +13,9 @@ const userSlice = createSlice({
     },
   },
 });
+export const userReducer = userSlice.reducer;
+export const { setCurrentUser } = userSlice.actions;
+
+export const selectCurrentUser = (state) => {
+  return state.user.currentUser;
+};
