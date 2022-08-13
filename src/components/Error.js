3d1b1d91040aadd21fe7e6946413386888320b9e
omaran bazna <omran4d@gmail.com>
2022-08-12 21:50:45 -0400

    finish error message component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,1 +1,13 @@
 import { Col } from "reactstrap";
+
+import React from "react";
+
+const Error = ({ errMsg }) => {
+  return (
+    <Col>
+      <h4>{errMsg}</h4>
+    </Col>
+  );
+};
+
+export default Error;
