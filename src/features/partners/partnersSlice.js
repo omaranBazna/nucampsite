bd4497848391d5b826fda13df382013077a8c1e8
omaran bazna <omran4d@gmail.com>
2022-08-12 22:39:41 -0400

    fetch partners array

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -1,11 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { PARTNERS } from "../../app/shared/PARTNERS";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { baseUrl } from "../../app/shared/baseUrl";
+import { mapImageURL } from "../../utils/mapImageURI";
+
+createAsyncThunk("campsites/fetchPartners", async () => {
+  const response = await fetch(baseUrl + "partners");
+  if (!response.ok) {
+    return Promise.reject("failed to fetch status") + response.status;
+  }
+  const data = await response.json();
+  return data;
+});
+
 const initialState = {
-  partnersArray: PARTNERS,
+  partnersArray: [],
+  errMsg: "",
+  isLoading: true,
 };
+const [pending, fulfilled, rejected] = fetchPartners;
 const partnersSlice = createSlice({
   name: "partners",
   initialState,
+  reducers: {},
+  extraReducers: {
+    pending: (state, action) => {
+      state.isLoading = true;
+    },
+    fulfilled: (state, action) => {
+      state.partnersArray = action.payload;
+      state.isLoading = false;
+    },
+    rejected: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = action.error ? action.error : "Error";
+    },
+  },
 });
 export const partnersReducer = partnersSlice.reducer;
 
