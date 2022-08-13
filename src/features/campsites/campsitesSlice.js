f79dea759b19388138c279d5d694212f58bf6c7c
omaran bazna <omran4d@gmail.com>
2022-08-12 21:37:39 -0400

    set up store to fetch campsites

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,8 +1,24 @@
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
-import { createSlice } from "@reduxjs/toolkit";
+//import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { baseUrl } from "../../app/shared/baseUrl";
+import { mapImageURL } from "../../utils/mapImageURI";
+
+export const fetchCampsites = createAsyncThunk(
+  "campsite/fetchCampsites",
+  async () => {
+    response = await fetch(baseUrl + "campsites");
+    if (!response.ok) {
+      return Promise.reject("unable to fetch ,status" + response.status);
+    }
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-  campsitesArray: CAMPSITES,
+  campsitesArray: [],
+  isLoading: true,
+  errMsg: "",
 };
 const campsitesSlice = createSlice({
   name: "campsites",
